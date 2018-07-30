ef773305b3b1f1f8a9f8dd0f7eda1450c79c43f6
Eric Langlais <ericlanglaismm@gmail.com>
2018-07-29 23:53:16 -0400

    add form for messages and show incoming messages in browser

--- server/server.js
+++ server/server.js
@@ -23,8 +23,9 @@ io.on('connection', (socket) => {
   socket.broadcast.emit("newMessage", generateMessage("Admin", "New user Joined"));
 
   
-  socket.on('createMessage', (message) =>{
+  socket.on('createMessage', (message, callback) =>{
     io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('this is from the server');
   });
   
   socket.on('disconnect', () => {
